b91b685931dab93098470b4ae06153a938560466
Tomasz Gordzielik <tomasz.gordzielik@gmail.com>
2020-03-28 09:45:36 +0100

    Add project files.

---
+++ CT-SCAN-Detection-tool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CT_SCAN_Detection_tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
